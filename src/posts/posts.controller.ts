129e8d068e652c19b2c66b8fa13a4c2ed3f2ab8e
Dwi Kuntarto <dwikuntarto.work@gmail.com>
2021-03-27 06:39:26 +0700

    Controllers, routing and the module structure

---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,34 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import CreatePostDto from './dto/create.post.dto';
+import UpdatePostDto from './dto/update.post.dto';
+import { PostsService } from './posts.service';
+
+@Controller('posts')
+export class PostsController {
+    constructor(private readonly postsService: PostsService) {}
+
+    @Get()
+    async getAllPosts() {
+        return this.postsService.getAllPosts();
+    }
+
+    @Get(':id')
+    async getPostById(@Param('id') id: string) {
+        return this.postsService.getPostById(Number(id));
+    }
+
+    @Post()
+    async createPost(@Body() post: CreatePostDto) {
+        return this.postsService.createPost(post);
+    }
+
+    @Put(':id')
+    async replacePost(@Param('id') id: string, @Body() post: UpdatePostDto) {
+        return this.postsService.replacePost(Number(id), post);
+    }
+
+    @Delete(':id')
+    async deletePost(@Param('id') id: string) {
+        return this.postsService.deletePost(Number(id));
+    }
+}
